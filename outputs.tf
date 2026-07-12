edcb9eb56843a66ec1f345f55665b0a6d70ef38c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "api_management_global_schemas" {
-  description = "All api_management_global_schema resources"
-  value       = azurerm_api_management_global_schema.api_management_global_schemas
-}
 output "api_management_global_schemas_api_management_name" {
-  description = "List of api_management_name values across all api_management_global_schemas"
-  value       = [for k, v in azurerm_api_management_global_schema.api_management_global_schemas : v.api_management_name]
+  description = "Map of api_management_name values across all api_management_global_schemas, keyed the same as var.api_management_global_schemas"
+  value       = { for k, v in azurerm_api_management_global_schema.api_management_global_schemas : k => v.api_management_name }
 }
 output "api_management_global_schemas_description" {
-  description = "List of description values across all api_management_global_schemas"
-  value       = [for k, v in azurerm_api_management_global_schema.api_management_global_schemas : v.description]
+  description = "Map of description values across all api_management_global_schemas, keyed the same as var.api_management_global_schemas"
+  value       = { for k, v in azurerm_api_management_global_schema.api_management_global_schemas : k => v.description }
 }
 output "api_management_global_schemas_resource_group_name" {
-  description = "List of resource_group_name values across all api_management_global_schemas"
-  value       = [for k, v in azurerm_api_management_global_schema.api_management_global_schemas : v.resource_group_name]
+  description = "Map of resource_group_name values across all api_management_global_schemas, keyed the same as var.api_management_global_schemas"
+  value       = { for k, v in azurerm_api_management_global_schema.api_management_global_schemas : k => v.resource_group_name }
 }
 output "api_management_global_schemas_schema_id" {
-  description = "List of schema_id values across all api_management_global_schemas"
-  value       = [for k, v in azurerm_api_management_global_schema.api_management_global_schemas : v.schema_id]
+  description = "Map of schema_id values across all api_management_global_schemas, keyed the same as var.api_management_global_schemas"
+  value       = { for k, v in azurerm_api_management_global_schema.api_management_global_schemas : k => v.schema_id }
 }
 output "api_management_global_schemas_type" {
-  description = "List of type values across all api_management_global_schemas"
-  value       = [for k, v in azurerm_api_management_global_schema.api_management_global_schemas : v.type]
+  description = "Map of type values across all api_management_global_schemas, keyed the same as var.api_management_global_schemas"
+  value       = { for k, v in azurerm_api_management_global_schema.api_management_global_schemas : k => v.type }
 }
 output "api_management_global_schemas_value" {
-  description = "List of value values across all api_management_global_schemas"
-  value       = [for k, v in azurerm_api_management_global_schema.api_management_global_schemas : v.value]
+  description = "Map of value values across all api_management_global_schemas, keyed the same as var.api_management_global_schemas"
+  value       = { for k, v in azurerm_api_management_global_schema.api_management_global_schemas : k => v.value }
 }
 
